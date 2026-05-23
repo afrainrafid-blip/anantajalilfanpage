ab59e2489f40246bfe6453c78fc8abaa3b8a8067
afrainrafid-blip <afrainrafid@gmail.com>
2026-05-23 15:24:12 +0600

    Add showLegend function to display alert and rotate body

---
+++ script.js
@@ -0,0 +1,11 @@
+function showLegend(){
+
+  alert("🔥 IMPOSSIBLE BOLE KICHU NAI 🔥");
+
+  document.body.style.transform = "rotate(1deg)";
+
+  setTimeout(() => {
+    document.body.style.transform = "rotate(-1deg)";
+  }, 200);
+
+}
